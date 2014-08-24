2d33ca48f1e59a78de55a512cb7076939d1cff91
FosterCraft <foster@fostermc.com>
2014-08-24 14:26:56 -0700

    Initial Commit.
    Change-Id: I8ae56e81d9d6ec7c387bedb9f8113e52bcdf33e3
    Signed-off-by: Foster <foster@fostermc.com>

---
+++ FosterCraft/src/main/java/me/Foster/Main.java
@@ -0,0 +1,25 @@
+package me.Foster;
+
+/*
+ * Welcome to the FosterCraft Source Repository.
+ * 
+ * I assume you're going to be reading this class first,
+ * so here's a little info for you. I'm going to out do my
+ * best to comment everything so you can understand the code,
+ * but if I fail to comment something (on-the-fly fixes, etc.)
+ * feel free to let me know.
+ */
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	// Called when FosterCraft is Started
+	public void onEnable() {
+		//
+	}
+	
+	// Called when FosterCraft is Stopped
+	public void onDisable() {
+		
+	}
+}
